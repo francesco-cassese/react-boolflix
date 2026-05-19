b7ef7b0cb2e0e69cbba1093e5881fc9f108d8563
Francesco Cassese <francescocassese9@gmail.com>
2026-05-19 02:15:24 +0200

    feat: aggiunta logica movieToShow per differenziare i film da mostrare

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,11 +3,18 @@ import { MovieContext } from "../contexts/MovieContext";
 import useSearchMovies from "../hooks/useSearchMovies";
 import CardMovie from "./CardMovie";
 import Loader from "./Loader";
+import useFetch from "../hooks/useFetch";
+import useTrendingMovies from "../hooks/useTrendingMovies";
+
+const URL_TRENDING_MOVIE = 'https://api.themoviedb.org/3/trending/movie/week'
 
 function Main() {
     const { query } = useContext(MovieContext);
 
-    const { movies, error, isLoading } = useSearchMovies(query);
+    const { movies, isLoading, error } = useSearchMovies(query);
+    const { trendingMovies } = useTrendingMovies();
+
+    const moviesToShow = query ? movies : trendingMovies;
 
     if (error) {
         return <p>Si è verificato un errore!</p>;
@@ -16,16 +23,15 @@ function Main() {
     return (
         <section>
             <h1>
-                {query ? "I film trovati" : "Film popolari del momento"}
+                {query ? "Film trovati" : "Film di tendenza"}
             </h1>
-
             <div className="container d-flex overflow-x-auto gap-3 py-3">
                 {isLoading ? (
                     <Loader />
                 ) : (
-                    movies.map(movie => (
-                        <article key={movie.id}>
-                            <CardMovie movie={movie} />
+                    moviesToShow.map(item => (
+                        <article key={item.id}>
+                            <CardMovie movie={item} />
                         </article>
                     ))
                 )}

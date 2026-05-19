e2a728e0574d7c0f619379ed270fe06c36cad9b3
Francesco Cassese <francescocassese9@gmail.com>
2026-05-19 15:43:50 +0200

    refactor: aggiunto reset css e personalizzato ulteriormente l'header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import styles from "./Header.module.css";
 function Header() {
 
     return (
-        <header className="d-flex align-items-center">
+        <header className="d-flex justify-content-between align-items-center bg-dark">
             <img src="/boolflix.png" alt="logo" className={styles.headerLogo} />
             <SearchBar />
         </header>

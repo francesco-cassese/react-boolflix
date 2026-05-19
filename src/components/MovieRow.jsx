05bb837c921e88c82eabdd37cb400db9e1dda440
Francesco Cassese <francescocassese9@gmail.com>
2026-05-19 19:43:40 +0200

    feat: aggiunti effetti allo scroll

--- src/components/MovieRow.jsx
+++ src/components/MovieRow.jsx
@@ -1,11 +1,12 @@
 import CardMovie from "./CardMovie";
+import style from "./MovieRow.module.css";
 
 function MovieRow({ title = "", movies, variant = "full" }) {
     return (
         <section className="mb-4">
             <h2 className="text-white mb-2">{title}</h2>
 
-            <div className="d-flex overflow-x-auto gap-2 pb-2">
+            <div className={`d-flex overflow-x-auto gap-2 pb-2 ${style.rowP} `}>
 
                 {movies?.map(movie => (
                     <div key={movie.id}>

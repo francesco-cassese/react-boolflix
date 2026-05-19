3a894bb0feecd1c04ca07679c129c22784751fa7
Francesco Cassese <francescocassese9@gmail.com>
2026-05-19 12:36:26 +0200

    feat: aggiunta immagine placeholder in caso di copertina mancante

--- src/components/CardMovie.jsx
+++ src/components/CardMovie.jsx
@@ -1,27 +1,33 @@
 import langToCountry from "../utils/languageMap";
 import styles from "./CardMovie.module.css";
 
-function CardMovie({ movie: item }) {
+function CardMovie({ movie }) {
 
-    const country = langToCountry(item.originalLanguage);
+    const country = langToCountry(movie.originalLanguage);
 
     return (
-        <div className="card h-100">
-            <h1>{item.title}</h1>
+        <div className="card h-100 position-relative">
+            <h4>{movie.title}</h4>
+            <span className={`badge position-absolute top-0 end-0 ${movie.type === 'movie' ? 'bg-success' : 'bg-danger'}`}>{movie.type}</span>
             <img
-                src={`https://image.tmdb.org/t/p/w342${item.poster}`}
-                alt={item.title}
+                src={
+                    movie.poster
+                        ? `https://image.tmdb.org/t/p/w342${movie.poster}`
+                        : '/placeholder.svg'
+                }
+                alt={movie.title}
             />
-            <h2>{item.originalTitle}</h2>
-            {country ? (<img
-                src={`https://flagcdn.com/${country}.svg`}
-                alt={item.originalLanguage}
-                className={styles.countryFlag}
-            />) : (
-                <span>🌐{item.originalLanguage}</span>
-            )
+            <h2>{movie.originalTitle}</h2>
+            {
+                country ? (<img
+                    src={`https://flagcdn.com/${country}.svg`}
+                    alt={movie.originalLanguage}
+                    className={styles.countryFlag}
+                />) : (
+                    <span>🌐{movie.originalLanguage}</span>
+                )
             }
-            <span>{item.rating}</span>
+            <span>{movie.rating}</span>
         </div >
     )
 }

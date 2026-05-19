0a99c383a22533ac7dbdd4c1129cb67f5c3efc6e
Francesco Cassese <francescocassese9@gmail.com>
2026-05-19 02:14:40 +0200

    refeat: modificata logica fetch della useSearchMovies

--- src/hooks/useSearchMovies.js
+++ src/hooks/useSearchMovies.js
@@ -1,20 +1,75 @@
-import useFetch from "./useFetch";
+import { useEffect, useState } from "react";
 
 function useSearchMovies(query) {
-    const URL_MOVIES = query
-        ? `https://api.themoviedb.org/3/search/movie?query=${encodeURI(query)}`
-        : 'https://api.themoviedb.org/3/movie/popular';
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-    const { data, isLoading, error } = useFetch(URL_MOVIES);
+    useEffect(() => {
+        if (!query) {
+            setMovies([]);
+            return;
+        }
 
-    const movies = (data ?? []).map(movie => ({
-        id: movie.id,
-        title: movie.title,
-        originalTitle: movie.original_title,
-        poster: movie.poster_path,
-        rating: movie.vote_average,
-        originalLanguage: movie.original_language,
-    }));
+        const token = import.meta.env.VITE_TMDB_API_KEY;
+
+        const options = {
+            method: "GET",
+            headers: {
+                accept: "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        };
+
+        const urlMovies = `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(query)}`;
+        const urlTv = `https://api.themoviedb.org/3/search/tv?query=${encodeURIComponent(query)}`;
+
+        setIsLoading(true);
+        setError(null);
+
+        Promise.all([
+            fetch(urlMovies, options).then(response => {
+                if (!response.ok) throw new Error("Errore proveniente da Movies");
+                return response.json();
+            }),
+            fetch(urlTv, options).then(response => {
+                if (!response.ok) throw new Error("Errore proveniente da Tv");
+                return response.json();
+            }),
+        ])
+            .then(([moviesData, tvData]) => {
+                const moviesMapped = (moviesData.results ?? []).map((item) => ({
+                    id: item.id,
+                    title: item.title,
+                    originalTitle: item.original_title,
+                    poster: item.poster_path,
+                    rating: item.vote_average,
+                    originalLanguage: item.original_language,
+                    type: "movie",
+                }));
+
+                const tvMapped = (tvData.results ?? []).map((item) => ({
+                    id: item.id,
+                    title: item.name,
+                    originalTitle: item.original_name,
+                    poster: item.poster_path,
+                    rating: item.vote_average,
+                    originalLanguage: item.original_language,
+                    type: "tv",
+                }));
+
+                const mergedList = [...moviesMapped, ...tvMapped];
+
+                setMovies(mergedList);
+                setIsLoading(false)
+            })
+            .catch(error => {
+                setError(error.message);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
+    }, [query]);
 
     return { movies, isLoading, error };
 }
